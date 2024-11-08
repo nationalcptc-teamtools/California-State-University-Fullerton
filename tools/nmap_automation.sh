1a73b79f26f23cb5e0177c5718c1ff2a932b39a7
Allen Dai <130431901+daiallen07@users.noreply.github.com>
2024-11-08 15:34:04 -0800

    Update nmap_automation.sh

--- tools/nmap_automation.sh
+++ tools/nmap_automation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #get ip from ping sweep
 nmap -sn -iL iplist.txt -oG foundIp.txt; grep -v Down foundIp.txt; grep -oP '\b(?:\d{1,3}\.){3}\d{1,3}\b' foundIp.txt > foundIp_filtered.txt
 rm iplist.txt
@@ -10,9 +10,9 @@ echo "Ping sweep complete"
 #after default ports are scanned come back and do -p-
 #if want to use -p-, must use --min-rate
 #nmap -sC -sV -iL foundIp_filtered.txt -Pn -oN nmap_output -oX xml_ouput
-nmap -iL foundIp_filtered.txt -Pn -oN nmap_output -oX xml_ouput
+nmap -iL foundIp_filtered.txt -Pn -oA nmap_output
 echo "Nmap complete"
 
 #convert xml into HTML
-xsltproc xml_output.xml > html_output.html
+xsltproc nmap_output.xml > nmap_output.html
 echo "html file created"
